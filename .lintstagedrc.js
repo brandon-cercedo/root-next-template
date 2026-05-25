28d22e52e2ecf9730a7b685f88d7b20b9ecf54de
Cercedo <marloncercedo@gmail.com>
2026-05-25 18:43:12 -0500

    chore: add `lint-staged`

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+const path = require("path"); // eslint-disable-line @typescript-eslint/no-require-imports
+
+const buildEslintCommand = (filenames) =>
+  `eslint --fix ${filenames
+    .map((f) => `"${path.relative(process.cwd(), f)}"`)
+    .join(" ")}`;
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand, "pnpm format"],
+};
